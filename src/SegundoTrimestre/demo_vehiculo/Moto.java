cc1b218bc28b832aae4b95808877f4edb56b309d
FilipLand <filip9038@gmail.com>
2024-01-29 12:04:15 +0100

    Segundo trimestre "tort(Para Mi)" Programción

--- src/SegundoTrimestre/demo_vehiculo/Moto.java
+++ src/SegundoTrimestre/demo_vehiculo/Moto.java
@@ -4,4 +4,44 @@ public class Moto extends Vehiculo implements IVehiculoMotor {
     public Moto(int nRuedas, int nAsientos) {
         super(nRuedas, nAsientos);
     }
+
+    @Override
+    public void cambiarMarcha(int marcha) {
+
+    }
+
+    @Override
+    public void cambiarMarcha() {
+
+    }
+
+    @Override
+    public float getMaxVel() {
+        return 0;
+    }
+
+    @Override
+    public void detener() {
+
+    }
+
+    @Override
+    public void encenderDerecha() {
+
+    }
+
+    @Override
+    public void encenderIzquierda() {
+
+    }
+
+    @Override
+    public void encederTodos() {
+
+    }
+
+    @Override
+    public void avagar() {
+
+    }
 }
--- src/SegundoTrimestre/demo_vehiculo/Starter.java
+++ src/SegundoTrimestre/demo_vehiculo/Starter.java
@@ -1,9 +1,9 @@
 package SegundoTrimestre.demo_vehiculo;
 
 public class Starter implements  {
-    public void startEngine(IVehiculoMotor IVehiculoMotor) {
+    public static void startEngine(IVehiculoMotor IVehiculoMotor) {
         IVehiculoMotor.arrancar();
         Starter starter = new Starter();
-        starter.startEngine();
+        startEngine();
     }
 }
