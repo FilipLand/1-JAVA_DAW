ff9868b97a2896fce12d02f434dde0c3d412ee03
FilipLand <filip9038@gmail.com>
2024-01-30 08:36:55 +0100

    Segundo trimestre "tort(Para Mi)" Programción

---
+++ src/SegundoTrimestre/OtroEjercicio/Test.java
@@ -0,0 +1,4 @@
+package SegundoTrimestre.OtroEjercicio;
+
+public class Test {
+}
--- src/SegundoTrimestre/demo_vehiculo/Coche.java
+++ src/SegundoTrimestre/demo_vehiculo/Coche.java
@@ -2,7 +2,7 @@ package SegundoTrimestre.demo_vehiculo;
 
 public class Coche extends Vehiculo implements IRadio, IVehiculoMotor {
 
-    public Coche(int nRuedas, int nAsientos, boolean tieneGps) {
+    public Coche(int nRuedas, int nAsientos) {
         super(nRuedas, nAsientos);
     }
 
@@ -33,6 +33,41 @@ public class Coche extends Vehiculo implements IRadio, IVehiculoMotor {
 
     @Override
     public void activarAntena() {
+
+    }
+
+    @Override
+    public void cambiarMarcha(int marcha) {
+
+    }
+
+    @Override
+    public void cambiarMarcha() {
+
+    }
+
+    @Override
+    public void acelerar() {
+        IVehiculoMotor.super.acelerar();
+    }
+
+    @Override
+    public void encenderDerecha() {
+
+    }
+
+    @Override
+    public void encenderIzquierda() {
+
+    }
+
+    @Override
+    public void encederTodos() {
+
+    }
+
+    @Override
+    public void avagar() {
 
     }
 }
