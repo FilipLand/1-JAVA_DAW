1f3f2999407fad963b68b6e5f5aa989ddb49b0a2
FilipLand <filip9038@gmail.com>
2024-01-30 08:13:33 +0100

    Segundo trimestre "tort(Para Mi)" Programción

---
+++ src/SegundoTrimestre/demo_vehiculo/Imusicplayer.java
@@ -0,0 +1,8 @@
+package SegundoTrimestre.demo_vehiculo;
+
+public interface Imusicplayer {
+    public void start();
+    public void stop();
+
+
+}
--- src/SegundoTrimestre/demo_vehiculo/Smartphone.java
+++ src/SegundoTrimestre/demo_vehiculo/Smartphone.java
@@ -1,8 +1,33 @@
 package SegundoTrimestre.demo_vehiculo;
 
-public class Smartphone implements IGPS {
+public class Smartphone implements IGPS, IRadio, Imusicplayer {
     @Override
     public void getCoordinates() {
+
+    }
+
+    @Override
+    public void startRadio() {
+
+    }
+
+    @Override
+    public void stopRadio() {
+
+    }
+
+    @Override
+    public void activarAntena() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
 
     }
 }
